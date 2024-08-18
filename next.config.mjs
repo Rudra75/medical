b3592bcf0682afc82ebded57490d8d35de2a4527
Rudra75 <101784599+Rudra75@users.noreply.github.com>
2024-08-18 23:35:46 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -14,7 +14,7 @@ export default withSentryConfig(withSentryConfig(nextConfig, {
 // https://github.com/getsentry/sentry-webpack-plugin#options
 
 org: "js-mastery-ya",
-project: "rudra_pro",
+project: "javascript-nextjs",
 
 // Only print logs for uploading source maps in CI
 silent: !process.env.CI,
